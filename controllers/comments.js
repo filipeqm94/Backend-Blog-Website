964ebf885fa06df1be8980c5225be068c7680ae3
filipeqm94 <queiroz.filipe94@gmail.com>
2021-12-24 19:27:52 -0500

    add middleware and auth to actions

--- controllers/comments.js
+++ controllers/comments.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const Article = require('../models/article')
 const Comments = require('../models/comment')
+const auth = require('../middleware/auth')
 
 const CommentsRouter = express.Router()
 
@@ -10,13 +11,7 @@ CommentsRouter.get('/', (req, res, next) => {
     .catch(next)
 })
 
-CommentsRouter.get('/:id', (req, res, next) => {
-  Comments.findById({ _id: req.params.id })
-    .then(comment => res.json(comment))
-    .catch(next)
-})
-
-CommentsRouter.post('/', (req, res, next) => {
+CommentsRouter.post('/', auth, (req, res, next) => {
   const { comment, articleId } = req.body
   Comments.create(comment).then(comment => {
     Article.findByIdAndUpdate(
@@ -29,13 +24,13 @@ CommentsRouter.post('/', (req, res, next) => {
   })
 })
 
-CommentsRouter.patch('/:id', (req, res, next) => {
+CommentsRouter.patch('/:id', auth, (req, res, next) => {
   Comments.findOneAndUpdate({ _id: req.params.id }, req.body, { new: true })
     .then(comment => res.json(comment))
     .catch(next)
 })
 
-CommentsRouter.delete('/:id', (req, res, next) => {
+CommentsRouter.delete('/:id', auth, (req, res, next) => {
   Comments.findOneAndDelete({ _id: req.params.id })
     .then(comment => res.json(comment))
     .catch(next)
